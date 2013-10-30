e8fe3d4f2ce131569596e355dffe9d25df2b6072
Reinierstam <reinierstam@gmail.com>
2013-10-30 11:33:12 +0100

    Added achievements

---
+++ application/controllers/Achievement.php
@@ -0,0 +1,12 @@
+<?php
+class Achievement extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+	$this->load->view('achievementView');
+    }
+}
+?>
---
+++ application/views/achievementView.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <title>achievments</title>
+            <style type="text/css">
+                .topBar {
+                    background-color: #83c8f9;
+                    border: 2px solid;
+                    border-color: #2984c3;
+                    height: 75px;
+                    width: 100%;
+                }
+                .greatdiv{
+                    background-color: #ceceed; 
+                    border: 2px solid;
+                    border-color: #2984c3;
+                    width: 98%;
+                    height: 750px;
+                    margin: 1%;
+                }
+				.smalldiv{
+					background-color: #ceceed; 
+                    border: 2px solid;
+                    border-color: #2984c3;
+                    width: 22%;
+                    height: 150px;
+                    margin: 1%;
+					float: left;
+				}
+				
+            </style>
+			
+			
+    </head>
+    <body>
+			
+        <div class ="topBar">TODO write content</div>
+        <div>
+            <div class ="greatdiv">
+				
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div> </br>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div> </br>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div> </br>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div>
+				<div class ="smalldiv"> hello </div> </br>
+				
+            </div>
+			
+    </body>
+</html>
